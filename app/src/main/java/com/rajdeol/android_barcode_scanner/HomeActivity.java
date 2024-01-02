e7c23dc79918d52af31e497583386334d5e5b0d6
Rajinder Pal Singh <RajinderPal.Singh@team.telstra.com>
2024-01-02 17:39:14 +1100

    updated old dependencies and compiled based on new version of gradle and android

--- app/src/main/java/com/rajdeol/android_barcode_scanner/HomeActivity.java
+++ app/src/main/java/com/rajdeol/android_barcode_scanner/HomeActivity.java
@@ -1,7 +1,11 @@
-package in.whomeninja.android_barcode_scanner;
+package com.rajdeol.android_barcode_scanner;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,9 +17,12 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 
-public class HomeActivity extends ActionBarActivity {
+public class HomeActivity extends AppCompatActivity {
     private String codeFormat,codeContent;
     private TextView formatTxt, contentTxt;
+
+    // Identifier for the permission request
+    private static final int USE_CAMERA_PERMISSIONS_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +61,9 @@ public class HomeActivity extends ActionBarActivity {
      * @param view view of the activity
      */
     public void scanNow(View view){
+        // check if user has grated the Camera permission
+        getPermissionForCamera();
+
         IntentIntegrator integrator = new IntentIntegrator(this);
         integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
         integrator.setPrompt(this.getString(R.string.scan_bar_code));
@@ -86,6 +96,50 @@ public class HomeActivity extends ActionBarActivity {
         }else{
             Toast toast = Toast.makeText(getApplicationContext(),"No scan data received!", Toast.LENGTH_SHORT);
             toast.show();
+        }
+    }
+
+    public void getPermissionForCamera(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // If the permission is NOT already granted.
+            // Check if the user has denied the permissions
+            // If so, we need to give more details about why we need the permission.
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show our own UI to explain to the user why we need to read the contacts
+                // before actually requesting the permission and showing the default UI
+            }
+
+            // Fire off an async request to actually get the permission
+            // This will show the standard permission request dialog UI
+            requestPermissions(new String[]{Manifest.permission.CAMERA},
+                    USE_CAMERA_PERMISSIONS_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        // Make sure it's our original use CAMERA request
+        if (requestCode == USE_CAMERA_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Use Camera permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                // showRationale = false if user clicks Never Ask Again, otherwise true
+                boolean showRationale = shouldShowRequestPermissionRationale( Manifest.permission.CAMERA);
+
+                if (showRationale) {
+                    // do something here to handle degraded mode
+                } else {
+                    Toast.makeText(this, "Read Contacts permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
